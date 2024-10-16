9ffbe3d51787bc67f10acca74be7a5b1c4b1170a
Khasim Hussain <khasimhussain.shaik@gmail.com>
2024-10-16 14:27:57 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,44 @@ function App() {
   return (
     <>
       <div className="flex flex-col md:flex-row-reverse gap-6">
-        <div className="bg-[#ffffff] text-black w-52 h-52 md: w-72 rounded-xl ">
+        <div className="flex flex-col items-center bg-[#ffffff] text-black w-52 h-52 md: w-72 rounded-xl ">
           <h2 className="text-red-900 font-bold text-2xl pt-3 ">
             Your Cart (0)
           </h2>
+          <img
+            src="/assets/rb_82226.png"
+            alt=""
+            className="transition animate-bounce pt-6 w-24 text-center"
+          />
+          <p className="text-red-900 text-xs pt-1">
+            Your added items will appear here...
+          </p>
         </div>
         <div className="flex flex-col items-start">
           <div>
-            <h2 className="text-4xl pl-5 font-extrabold">Desserts</h2>
+            <h2
+              className="text-4xl pl-5 font-extrabold dark:text-black
+            "
+            >
+              Desserts
+            </h2>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-black">
             {/* card-1 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className=" relative  transition duration-300 ease-in-out hover:scale-110 ">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+                <span className=" hover:transition animate-bounce absolute -right-3 -top-2 bg-green-500 text-xs p-1 rounded-lg ">
+                  New
+                </span>
+              </div>
+
               <div className="">
-                <button className="text-black border-stone-950 px-6 py-1 text-sm rounded-3xl ">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -36,14 +55,15 @@ function App() {
             </div>
             {/* card-2 */}
             <div className="p-6 rounded-xl">
-              {" "}
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-105">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -57,14 +77,15 @@ function App() {
             </div>
             {/* card-3 */}
             <div className="p-6 rounded-xl">
-              {" "}
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -78,13 +99,18 @@ function App() {
             </div>
             {/* card-4 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="relative transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+                <span className=" hover:transition animate-spin absolute -right-3 -top-2 bg-red-400 px-2  ">
+                  *
+                </span>
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -98,13 +124,15 @@ function App() {
             </div>
             {/* card-5 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -118,13 +146,15 @@ function App() {
             </div>
             {/* card-6 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -138,13 +168,15 @@ function App() {
             </div>
             {/* card-7 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -158,13 +190,15 @@ function App() {
             </div>
             {/* card-8 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
@@ -178,13 +212,15 @@ function App() {
             </div>
             {/* card-9 */}
             <div className="p-6 rounded-xl">
-              <img
-                src="/assets/2148352300.jpg"
-                alt=""
-                className="rounded-2xl w-60"
-              />
+              <div className="transition duration-500 ease-in-out hover:scale-110">
+                <img
+                  src="/assets/2148352300.jpg"
+                  alt=""
+                  className="rounded-2xl w-60"
+                />
+              </div>
               <div>
-                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl">
+                <button className="bg-white text-black border-stone-950 px-6 py-1 text-sm rounded-3xl hover:bg-green-800 hover:text-white transition-all duration-300 ease-in-out">
                   Add to Cart
                 </button>
               </div>
